747323b0230a6aa93c332b6c8c1b997bdcb66b27
Daniel Cregg <danielcregg@users.noreply.github.com>
2024-11-21 16:27:00 +0000

    added constructor

--- src/ie/atu/watchmanager/Watch.java
+++ src/ie/atu/watchmanager/Watch.java
@@ -7,6 +7,48 @@ public class Watch {
     private String brand;
     private float price;
     private boolean isSold;
+
+    // Constructor
+    public Watch(int serialNumber, String brand, float price, boolean isSold){
+        this.serialNumber = serialNumber;
+        this.brand = brand;
+        this.price = price;
+        this.isSold = isSold;
+    }
+
+    // Getters and Setters
+    public int getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(int serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public boolean isSold() {
+        return isSold;
+    }
+
+    public void setSold(boolean isSold) {
+        this.isSold = isSold;
+    }
+
     
 
 }
