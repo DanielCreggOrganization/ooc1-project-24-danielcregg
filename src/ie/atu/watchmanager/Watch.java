4eede21189d90badb7d43aaf239e9204842679f4
Daniel Cregg <danielcregg@users.noreply.github.com>
2024-11-21 16:00:43 +0000

    Added instance variables

--- src/ie/atu/watchmanager/Watch.java
+++ src/ie/atu/watchmanager/Watch.java
@@ -2,4 +2,11 @@ package ie.atu.watchmanager;
 
 public class Watch {
 
+    // Instance Variables
+    private int serialNumber; // Unique ID
+    private String brand;
+    private float price;
+    private boolean isSold;
+    
+
 }
